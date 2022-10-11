4ff8c031e4aa51a2910a0d329b554d1eff90a08f
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-11 07:10:19 -0400

    part 2 readFile working

--- urls.js
+++ urls.js
@@ -1,4 +1,91 @@
 const axios = require('axios');
 const fs = require('fs');
+const argv = process.argv;
 
 
+const readFile = argv[2];
+
+fs.readFile(readFile, 'utf8', function (err, data) {
+    if (err) {
+        console.error(`Could not read file: ${readFile}`, err);
+        process.kill(1);
+    }
+    urlArr = data.toString().split('\n')
+    console.log(urlArr);
+    return urlArr;
+});
+
+
+
+
+
+// fs.readFile(readFile, (err, data) => {
+//     if (err) {
+//         console.error(`Could not read file ${path}`, err);
+//         process.kill(1);
+//     }
+//     async function getData(urlsArr) {
+//         const urls = []
+
+//         for (url of urlsArr) {
+//             urls.push(url)
+//         }
+
+//         const promises = urls.map((url) =>
+//             axios.get(url)
+//                 .then((response) => response.data)
+//                 .catch(e => console.log("INVALID USERNAME", e))
+//         );
+//         const data = await Promise.all(promises);
+//         console.log(data);
+//         return data;
+//     }
+
+//     fs.appendFile(path, data, 'utf8', err => {
+//         if (err) {
+//             console.error("Could not save data", err);
+//             return;
+//         }
+//         console.log("IT WORKED!")
+//     })
+// });
+
+
+
+
+// fs.appendFile(path, data, 'utf8', err => {
+//     if (err) {
+//         console.log("ERROR!!!", err)
+//         process.kill(1)
+//     }
+//     console.log("IT WORKED!")
+// })
+
+
+// fs.appendFile(path, data, 'utf8', err => {
+//     if (err) {
+//         console.error("Could not save data", err);
+//         process.kill(1);
+//     }
+//     console.log("IT WORKED!")
+// })
+
+
+
+async function getUsers(usersArr) {
+    const baseUrl = 'https://api.github.com/users/'
+    const urls = []
+
+    for (user of usersArr) {
+        urls.push(baseUrl + user)
+    }
+
+    const promises = urls.map((url) =>
+        axios.get(url)
+            .then((response) => response.data)
+            .catch(e => console.log("INVALID USERNAME", e))
+    );
+    const data = await Promise.all(promises);
+    console.log(data);
+    return data;
+}
