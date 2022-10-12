a151f425b04fbcee13ec5dedaf10eab6bcaa1382
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-12 19:40:33 -0400

    part 2 done

--- urls.js
+++ urls.js
@@ -3,110 +3,39 @@ const fs = require('fs');
 const argv = process.argv;
 
 
-const readFile = argv[2];
+function processFile(path) {
+    fs.readFile(path, 'utf8', async function (err, data) {
+        if (err) {
+            console.error(`Could not read file ${readFile}: ${err}`);
+            process.exit(1);
+        }
+        //add individual urls from file to new array, splitting at line break and removing any unusable spaces
+        let urlArr = data.split('\n').filter(u => u != '');
+        for (let url of urlArr) {
+            let resp;
 
-fs.readFile(readFile, 'utf8', function (err, data) {
-    if (err) {
-        console.error(`Could not read file: ${readFile}`, err);
-        process.kill(1);
-    }
-    urlArr = data.toString().split('\n')
+            try {
+                resp = (await axios.get(url)).data;
+            } catch {
+                console.error(`Invalid URL: ${url}`);
+                //this is what I was missing:
+                continue; //lets our code move on to the next url even if a url fails
+            }
 
-    async function writeFiles(urlArr) {
-        const promises = urlArr.map((url) =>
-            axios.get(url)
-                .then((response) => response.data)
-                .catch(e => console.log('Invalid URL'))
-        );
+            //reads and modifies the url
+            //returns a string of the url with only the domain name
+            let fileName = new URL(url).hostname; 
 
-        const data = await Promise.all(promises);
-        console.log(data);
-        return data;
-    }
-    writeFiles(urlArr);
-    // fs.appendFile(path, data, 'utf8', err => {
-    // if (err) {
-    //     console.log("ERROR!!!", err)
-    //     process.kill(1)
-    // }
-    // console.log("IT WORKED!")
-    // })
-
-    // console.log(urlArr);
-    // return urlArr;
-});
-
-
-
-
-
-// fs.readFile(readFile, (err, data) => {
-//     if (err) {
-//         console.error(`Could not read file ${path}`, err);
-//         process.kill(1);
-//     }
-//     async function getData(urlsArr) {
-//         const urls = []
-
-//         for (url of urlsArr) {
-//             urls.push(url)
-//         }
-
-//         const promises = urls.map((url) =>
-//             axios.get(url)
-//                 .then((response) => response.data)
-//                 .catch(e => console.log("INVALID USERNAME", e))
-//         );
-//         const data = await Promise.all(promises);
-//         console.log(data);
-//         return data;
-//     }
-
-//     fs.appendFile(path, data, 'utf8', err => {
-//         if (err) {
-//             console.error("Could not save data", err);
-//             return;
-//         }
-//         console.log("IT WORKED!")
-//     })
-// });
-
-
-
-
-// fs.appendFile(path, data, 'utf8', err => {
-//     if (err) {
-//         console.log("ERROR!!!", err)
-//         process.kill(1)
-//     }
-//     console.log("IT WORKED!")
-// })
-
-
-// fs.appendFile(path, data, 'utf8', err => {
-//     if (err) {
-//         console.error("Could not save data", err);
-//         process.kill(1);
-//     }
-//     console.log("IT WORKED!")
-// })
-
-
-
-async function getUsers(usersArr) {
-    const baseUrl = 'https://api.github.com/users/'
-    const urls = []
-
-    for (user of usersArr) {
-        urls.push(baseUrl + user)
-    }
-
-    const promises = urls.map((url) =>
-        axios.get(url)
-            .then((response) => response.data)
-            .catch(e => console.log("INVALID USERNAME", e))
-    );
-    const data = await Promise.all(promises);
-    console.log(data);
-    return data;
+            //writes the data to a new file if one doesn't exist or replaces the file is one exists
+            fs.writeFile(fileName, resp, 'utf8', err => {
+                if (err) {
+                    console.error(`Could not save data for ${fileName}`);
+                }
+                console.log(`Successfully created ${fileName}`);
+            });
+        }
+    });
 }
+
+//executes the function using the path name passed in the command line at index 2
+processFile(argv[2]);
