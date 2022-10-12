52e58410006722fa7a5711b340fb6ec6597503df
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-12 19:54:14 -0400

    fixed app files set up and npm initialized

---
+++ fixed-app/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const ExpressError = require("./expressError")
+
+
+app.post('/', function(req, res, next) {
+  try {
+    let results = req.body.developers.map(async d => {
+      return await axios.get(`https://api.github.com/users/${d}`);
+    });
+    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+    return res.send(JSON.stringify(out));
+  } catch {
+    next(err);
+  }
+});
+
+app.listen(3000);
